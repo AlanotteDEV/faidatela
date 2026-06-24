45d242d8288e600e5e5de125acc641e274eda07f
AntonioDEV <alanotte6@gmail.com>
2026-06-24 14:34:06 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// === CONFIGURAZIONE FIREBASE ===
+// Sostituisci questi valori con quelli del TUO progetto Firebase.
+// Li trovi in: Console Firebase -> Impostazioni progetto (icona ingranaggio) ->
+// Generali -> "Le tue app" -> app Web -> oggetto firebaseConfig
+
+const firebaseConfig = {
+  apiKey: "INSERISCI_API_KEY",
+  authDomain: "INSERISCI_PROJECT_ID.firebaseapp.com",
+  projectId: "INSERISCI_PROJECT_ID",
+  storageBucket: "INSERISCI_PROJECT_ID.appspot.com",
+  messagingSenderId: "INSERISCI_SENDER_ID",
+  appId: "INSERISCI_APP_ID"
+};
